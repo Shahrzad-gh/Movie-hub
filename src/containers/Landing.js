e7ba61a5d7a519c199f1c0261ccba8cd7c44d26d
Shahr Zad <gh.shahrzad@gmail.com>
2021-08-22 17:33:38 +0430

    update

--- src/containers/Landing.js
+++ src/containers/Landing.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import { makeStyles } from '@material-ui/core/styles';
 import ImageList from '@material-ui/core/ImageList';
@@ -6,6 +7,7 @@ import ImageListItem from '@material-ui/core/ImageListItem';
 import ImageListItemBar from '@material-ui/core/ImageListItemBar';
 import IconButton from '@material-ui/core/IconButton';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
+import { fetchPopular } from "../redux/actions/index"
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -34,13 +36,19 @@ const useStyles = makeStyles((theme) => ({
 
 function Landing() {
   const classes = useStyles();
+  const dispatch = useDispatch();
+  const movies = useSelector(state => state.movies)
+
+  useEffect(() => {
+    dispatch(fetchPopular)
+  }, [])
 
   return (
     <div className={classes.root}>
       <div>
       <div className={classes.root}>
       <ImageList className={classes.imageList} cols={2.5}>
-        {/* {itemData.map((item) => (
+        {/* {movies.popular.map((item) => (
           <ImageListItem key={item.img}>
             <img src={item.img} alt={item.title} />
             <ImageListItemBar
