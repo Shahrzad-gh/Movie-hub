1e3f5fe8058f52cbe1ef1f3ff2442a060a82b1e4
Shahr Zad <gh.shahrzad@gmail.com>
2021-08-25 12:45:54 +0430

    add header

---
+++ src/containers/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  handleRandomImage = () => {
+    return src
+  }
+  return (
+    <div>
+      <img src={handleRandomImage} />
+    </div>
+  )
+}
+
+export default Header
