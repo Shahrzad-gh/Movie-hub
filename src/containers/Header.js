b724f1a68700be28ce33ebe939ca28c74c345eca
Shahr Zad <gh.shahrzad@gmail.com>
2021-08-31 12:44:26 +0430

    update

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,17 +1,14 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import poster from "../poster.jpg";
-import Navbar from "../components/Navbar";
 const useStyles = makeStyles(() => ({
   root: {
     display: "block",
-    height: "100vh",
     overflow: "hidden",
+    background: "linear-gradient( #1d3557, #61dafb, #1d3557) ",
   },
   posterStyle: {
-    width: "100%",
-    background:
-      "linear-gradient(rgba(245, 246, 252, 0.52), rgba(117, 19, 93, 0.73))",
+    width: "auto",
   },
 }));
 
@@ -22,7 +19,6 @@ function Header() {
   // }
   return (
     <>
-      <Navbar />
       <div className={classes.root}>
         <img alt="poster" src={poster} className={classes.posterStyle} />
       </div>
