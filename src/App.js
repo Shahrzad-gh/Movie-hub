00717f8324218a7fb225a99a6ef4f51e13f2ce46
Shahr Zad <gh.shahrzad@gmail.com>
2021-08-31 12:43:07 +0430

    add router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import './App.css';
-import Landing from './containers/Landing';
-import Navbar from './components/Navbar';
+import "./App.css";
+import Landing from "./containers/Landing";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Details from "./containers/Details";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-      <div className="App">
-        <Landing />
-    </div>
-
+    <>
+      <Navbar />
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={Landing}></Route>
+          <Route path="/Details/:id" component={Details}></Route>
+        </div>
+      </Router>
+      <Footer />
+    </>
   );
 }
 
