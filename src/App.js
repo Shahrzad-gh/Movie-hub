76d22064f2b765a2a47f5552328e89a40d3f11b5
Shahr Zad <gh.shahrzad@gmail.com>
2021-09-02 11:32:30 +0430

    update router

--- src/App.js
+++ src/App.js
@@ -4,16 +4,27 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Details from "./containers/Details";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import NotFound from "./components/404";
 
 function App() {
   return (
     <>
       <Navbar />
       <Router>
-        <div className="App">
-          <Route exact path="/" component={Landing}></Route>
-          <Route path="/Details/:id" component={Details}></Route>
-        </div>
+        <Switch>
+          <div className="App">
+            <Router>
+              <Route exact path="/" component={Landing}></Route>
+            </Router>
+
+            <Router>
+              <Route path="/Details/:id" component={Details}></Route>
+            </Router>
+            <Route>
+              <NotFound />
+            </Route>
+          </div>
+        </Switch>
       </Router>
       <Footer />
     </>
