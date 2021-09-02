c4ebad419ae7aa4f7fc318bac7b9135c101f667b
Shahr Zad <gh.shahrzad@gmail.com>
2021-09-02 15:41:35 +0430

    get genre ids - redux

--- src/containers/Landing.js
+++ src/containers/Landing.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 //import { makeStyles } from '@material-ui/core/styles';
 import PopularMovies from "./PopularMovies";
 import PopularTvs from "./PopularTvs";
 import Header from "./Header";
 import Trendings from "./Trendings";
-import Footer from "../components/Footer";
+import { fetchGenres } from "../redux/actions/index";
+import { useDispatch, useSelector } from "react-redux";
+import Genre from "./Genre";
 
 function Landing() {
+  const dispatch = useDispatch();
+  const search = useSelector((state) => state.search);
+
+  useEffect(() => {
+    dispatch(fetchGenres());
+  }, [dispatch]);
+
   return (
     <div>
       <Header />
       <PopularMovies />
       <PopularTvs />
       <Trendings />
+      {search.genres &&
+        search.genres.map((item) => <Genre key={item.id} data={item} />)}
     </div>
   );
 }
